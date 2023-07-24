c49b334053b0b99de8df5084be3b1162898b7be5
K-Karalius <kristupaskaralius1@gmail.com>
2023-07-24 21:14:56 +0300

    maven wrapper and pom

--- src/main/java/com/app/streamapi/Main.java
+++ src/main/java/com/app/streamapi/Main.java
@@ -1,23 +1,7 @@
 package com.app.streamapi;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Program");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
+public class Main {
+    public static void main(String[] args){
+        App.main(args);
     }
 }
